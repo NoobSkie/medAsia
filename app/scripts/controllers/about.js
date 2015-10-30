aa55d7c9594455733c9137d99bde7adb0803ffe7
Tyler <mcnew1234@sbcglobal.net>
2015-10-30 17:21:42 +0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medAsiaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the medAsiaApp
+ */
+angular.module('medAsiaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medAsiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the medAsiaApp
+ */
+angular.module('medAsiaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
